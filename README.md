a6e807a73a0445258e5f39ffe888fb07da76103c
skywalker8p <61873877+skywalker8p@users.noreply.github.com>
2022-08-20 06:37:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# CPP-Graph-Library
+This library consists of *.h and *.cpp files, most-importantly: Graph.h and Graph.cpp. Including these files will allow the user to instantiate 'Graph' objects to model graph-related problems. Graphs are persistent (ie they can be written to disk as *.txt files for later retrieval through a read-from-file Graph constructor).
+
+No external libraries (eg Boost) were used in this project, except for the C++ Standard Template Library.
---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "Graph.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string edgeString(tuple<string, string, int>); //Useful for print edges
+
+int main() {
+
+  vector<string> nodes = {"A", "B", "C", "D"};
+  Graph G;
+  G.addNode(1.12314, "A");
+  G.addNode(-7.3412, "B");
+  G.addNode(420, "C");
+  G.addNode(-12423, "D");
+
+  G.addEdge("A","B", 1);
+  G.addEdge("B","C", -714234.322323);
+  G.addEdge("C","D", 313412341234123);
+  G.addEdge("D","A", -3);
+
+  G.saveGraph("Yee");
+  Graph F(string("Yee.txt"));
+
+  cout << G.getInfo();
+  cout << F.getInfo();
+
+
+  cout << "\n------------------------------------" << endl;
+  cout << "Done." << endl;
+  cout << "------------------------------------" << endl;
+}
+
+string edgeString(tuple<string, string, int> edge) {
+  string str = get<0>(edge) + get<1>(edge) + to_string(get<2>(edge));
+  return str;
+}
